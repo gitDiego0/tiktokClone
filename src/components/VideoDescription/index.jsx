7892fbf91fbf61358126c51fe0e94a64b15a919f
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-08 11:20:24 +0100

    feat(Root): video description component
    Component that renders description of the video and the album title

---
+++ src/components/VideoDescription/index.jsx
@@ -0,0 +1,31 @@
+import AlbumDisk from './AlbumDisk'
+import SongTicker from '../SongTicker/index'
+
+import styles from './styles.module.css'
+
+export default function VideoDescription({
+  author,
+  description,
+  albumCover,
+  songTitle,
+}) {
+  console.log(songTitle)
+  return (
+    <footer className={styles.description}>
+      <div className={styles.textWrapper}>
+        <section>
+          <strong>
+            <a className={styles.author} href={`/users/${author}`}>
+              @{author}
+            </a>
+          </strong>
+          <p className={styles.text}>{description}</p>
+        </section>
+        <SongTicker songTitle={songTitle} />
+      </div>
+      <div className={styles.albumImage}>
+        <AlbumDisk albumCover={albumCover} />
+      </div>
+    </footer>
+  )
+}
