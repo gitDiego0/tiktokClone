f12550089528ede5e6a66da74853b3dc1f936f0c
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-08 11:22:02 +0100

    fix(Root): import VideoDescription component

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,15 +1,22 @@
 import clsx from 'clsx'
 import { useState, useRef } from 'react'
 import styles from './styles.module.css'
+import VideoDescription from '../VideoDescription/index.jsx'
 import VideoPlayerActions from './VideoPlayerActions'
 
 // const SRC =
 //   'https://v16-webapp.tiktok.com/208de1130461e4512e97f6be17bab4b2/621cefb6/video/tos/useast2a/tos-useast2a-ve-0068c004/e653995ba7214d51866c5db1d38f1e43/?a=1988&br=3016&bt=1508&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-307nz7Thz7VcDXq&l=202202280951400102230811570BB9617B&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=am88ZmQ6ZnJ0OzMzNzczM0ApM2gzOzQ3ZDtpN2UzOWdkPGdjb2ZhcjRvLWFgLS1kMTZzc2EvNTYxNi9jNTBiMjVfYTM6Yw%3D%3D&vl=&vr="'
 
-export default function VideoPlayer({ src }) {
+export default function VideoPlayer({
+  songTitle,
+  author,
+  description,
+  albumCover,
+  src,
+}) {
   const [playing, setPlaying] = useState(false)
   const video = useRef(null)
-
+  VideoDescription
   const handlePlay = () => {
     const { current: videoEl } = video
 
@@ -23,16 +30,23 @@ export default function VideoPlayer({ src }) {
   })
 
   return (
-    <div className={styles.wrapper} onClick={handlePlay}>
+    <div className={styles.wrapper}>
       <video
         className={styles.video}
-        src={src}
-        loop
         controls={false}
+        loop
+        onClick={handlePlay}
         ref={video}
+        src={src}
       />
-      <span className={playerClassname} />
+      <span className={playerClassname} onClick={handlePlay} />
       <VideoPlayerActions />
+      <VideoDescription
+        author={author}
+        description={description}
+        albumCover={albumCover}
+        songTitle={songTitle}
+      />
     </div>
   )
 }
