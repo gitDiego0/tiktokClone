7a89b592e2c08a789af8b3039bf0509cd85b8f1f
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-03-15 20:22:24 +0100

    feat(Root): use of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import FeedVideos from './components/FeedVideos'
+import Upload from './pages/Upload/index'
+import { Route } from 'wouter'
 
 function App() {
   return (
     <div className="App">
       <main>
-        <FeedVideos />
+        <Route path="/">
+          <FeedVideos />
+        </Route>
+        <Route path="/upload">
+          <Upload />
+        </Route>
       </main>
     </div>
   )
